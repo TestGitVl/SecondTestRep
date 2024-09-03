458772b3485342243ca530d81c6e3c415a47c20c
VladislavPaddeus <paddeusvladislav@gmail.com>
2024-09-03 21:39:16 +0400

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите операцию: ");
+        String operation = in.next();
+
+        System.out.println("Введите 2 чиcла: ");
+        int number1 = in.nextInt();
+        int number2 = in.nextInt();
+
+        switch (operation){
+
+
+        }
+
+
+    }
+}
